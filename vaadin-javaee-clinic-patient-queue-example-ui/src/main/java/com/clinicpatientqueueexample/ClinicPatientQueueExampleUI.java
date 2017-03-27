d11e282dc7d13b41b63c6a52d6caee9ed786ef08
Mart Sõmermaa <mart.somermaa@gmail.com>
2017-03-28 00:46:23 +0300

    Use Navigator

--- vaadin-javaee-clinic-patient-queue-example-ui/src/main/java/com/clinicpatientqueueexample/ClinicPatientQueueExampleUI.java
+++ vaadin-javaee-clinic-patient-queue-example-ui/src/main/java/com/clinicpatientqueueexample/ClinicPatientQueueExampleUI.java
@@ -4,8 +4,10 @@ import com.clinicpatientqueueexample.data.CrudService;
 import com.clinicpatientqueueexample.patients.Patient;
 import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIUI;
+import com.vaadin.cdi.CDIViewProvider;
 import com.vaadin.data.provider.CallbackDataProvider;
 import com.vaadin.data.provider.DataProvider;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
@@ -13,37 +15,24 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+import javax.inject.Inject;
+
 @CDIUI("registration-kiosk")
 @Theme("clinicpatientqueueexampletheme")
 public class ClinicPatientQueueExampleUI extends UI {
 
-    private CrudService<Patient> service = new CrudService<>();
-    private DataProvider<Patient, String> dataProvider = new CallbackDataProvider<>(
-            query -> service.findAll().stream(), query -> service.findAll().size());
-    private int number;
+    @Inject
+    private CDIViewProvider cdiViewProvider;
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+        final VerticalLayout contentArea = new VerticalLayout();
+        contentArea.setMargin(false);
+        setContent(contentArea);
 
-        final Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            service.save(new Patient(String.valueOf(number++), name.getValue()));
-            dataProvider.refreshAll();
-        });
-
-        final Grid<Patient> grid = new Grid<>();
-        grid.addColumn(Patient::getName).setCaption("Name");
-        grid.setDataProvider(dataProvider);
-        grid.setSizeFull();
-
-        layout.addComponents(name, button, grid);
-        layout.setSizeFull();
-        layout.setExpandRatio(grid, 1.0f);
-
-        setContent(layout);
+        final Navigator navigator = new Navigator(this, contentArea);
+        navigator.addProvider(cdiViewProvider);
+        navigator.navigateTo(DefaultView.NAME);
     }
 
 }
---
+++ vaadin-javaee-clinic-patient-queue-example-ui/src/main/java/com/clinicpatientqueueexample/DefaultView.java
@@ -0,0 +1,46 @@
+package com.clinicpatientqueueexample;
+
+import com.clinicpatientqueueexample.data.CrudService;
+import com.clinicpatientqueueexample.patients.Patient;
+import com.vaadin.cdi.CDIView;
+import com.vaadin.data.provider.CallbackDataProvider;
+import com.vaadin.data.provider.DataProvider;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+@CDIView(DefaultView.NAME)
+public class DefaultView extends VerticalLayout implements View {
+
+    public static final String NAME = "default";
+
+    private CrudService<Patient> service = new CrudService<>();
+    private DataProvider<Patient, String> dataProvider = new CallbackDataProvider<>(
+            query -> service.findAll().stream(), query -> service.findAll().size());
+    private int number;
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+
+        final TextField name = new TextField();
+        name.setCaption("Type your name here:");
+
+        final Button button = new Button("Click Me");
+        button.addClickListener(e -> {
+            service.save(new Patient(String.valueOf(number++), name.getValue()));
+            dataProvider.refreshAll();
+        });
+
+        final Grid<Patient> grid = new Grid<>(Patient.class);
+        grid.setDataProvider(dataProvider);
+        grid.setSizeFull();
+
+        addComponents(name, button, grid);
+        setSizeFull();
+        setExpandRatio(grid, 1.0f);
+    }
+
+}
