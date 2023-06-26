d26346e8ae008db623644c205483a78f69e0f703
Mart Somermaa <mrts@users.noreply.github.com>
2023-06-26 20:31:26 +0300

    Use the new versionless context root in launch-apps.sh

--- launch-apps.sh
+++ launch-apps.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-VERSION="1.0.1"
+set -eu
 
 case "`uname`" in
 	MINGW*) CHROME_CMD='/c/Program Files (x86)/Google/Chrome/Application/chrome.exe';;
 	*) CHROME_CMD='google-chrome';;
 esac
 
-APP_PREFIX="--app=http://localhost:8080/vaadin-javaee-clinic-patient-queue-example-ui-$VERSION"
+APP_PREFIX="--app=http://localhost:8080/vaadin-javaee-clinic-patient-queue-example"
 
 for app in registration-kiosk doctors-office infoscreen
 do
